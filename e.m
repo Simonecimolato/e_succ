6d89578e6f3ade78760f954ce8971c9461fe543a
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2022-04-10 16:46:39 +0200

    Add files via upload

---
+++ e.m
@@ -0,0 +1,27 @@
+clear
+clc
+close all
+
+%% PARALLELIZATION
+%parpool;
+
+%%
+dim = 15; %max is 15
+base = 10;
+it = zeros(dim,1);
+
+for j = 1:dim
+    i = 1;
+    while exp(1)-((1+1/i)^i) > base^(-j)
+        i = i+1;
+    end
+    it(j) = i;
+    disp(j);
+end
+
+%%
+loglog(it);
+grid on;
+
+%%
+delete(gcp('nocreate')); %shutting down parallel pool
